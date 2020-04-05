2705a83edee8e7e2ea2284f77a9e455e727107af
Artak-Mkrtchyan <30382535+Artak-Mkrtchyan@users.noreply.github.com>
2020-04-05 21:15:36 +0400

    feat(query): created seeFullPost query

--- src/fragments.js
+++ src/fragments.js
@@ -1,14 +1,9 @@
-export const USER_FRAGMENT = `
-  fragment UserParts on User {
+export const COMMENT_FRAGMENT = `
+  fragment CommentParts on Comment {
     id,
-    username,
-    email,
-    firstName,
-    lastName,
-    bio,
-    post {
-      id
-      caption
+    text,
+    user {
+      username
     }
   }
 `;
