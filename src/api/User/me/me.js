492016415daf492439717ea26eaaccda6c867170
Artak <artak6297@gmail.com>
2020-05-19 01:09:32 +0400

    feat(subscribtion): created newMessage subscribtion

--- src/api/User/me/me.js
+++ src/api/User/me/me.js
@@ -8,7 +8,7 @@ export default {
 
       const { user } = request;
       const userProfile = await prisma.user({ id: user.id });
-      const posts = await prisma.user({ id: user.id }).post();
+      const posts = await prisma.user({ id: user.id }).posts();
 
       return {
         user: userProfile,
